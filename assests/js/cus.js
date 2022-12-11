4f327761db13b6bcfec1852d45807bab5c0d7284
WaiYanLatt <humanant71@gmail.com>
2022-12-11 11:26:56 +0630

    c

--- assests/js/cus.js
+++ assests/js/cus.js
@@ -1,21 +1,20 @@
 var waypoint = new Waypoint({
-    element: document.getElementById('about'),
-    handler: function(direction) {
-        if (direction === "down") {
-            document.querySelector(".navbar").classList.add("navbar-control")
-            document.querySelector(".navbar").classList.add("animate__fadeInDown")
-            document.querySelector(".navbar").classList.add("shadow")
-        } else {
-            document.querySelector(".navbar").classList.remove("navbar-control")
-            document.querySelector(".navbar").classList.remove("animate__fadeInDown")
-        }
-    },
-    offset: '10%'
-})
-
+  element: document.getElementById("about"),
+  handler: function (direction) {
+    if (direction === "down") {
+      document.querySelector(".navbar").classList.add("navbar-control");
+      document.querySelector(".navbar").classList.add("animate__fadeInDown");
+      document.querySelector(".navbar").classList.add("shadow");
+    } else {
+      document.querySelector(".navbar").classList.remove("navbar-control");
+      document.querySelector(".navbar").classList.remove("animate__fadeInDown");
+    }
+  },
+  offset: "10%",
+});
 
 // menu icon
-let menuIcon = document.querySelector('.menu-icon');
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('show');
-})
+let menuIcon = document.querySelector(".menu-icon");
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("show");
+});
